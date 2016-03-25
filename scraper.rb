ccfcfebbb0659a40a1b6b45c1000763ad3597830
Tony Bowden <tony@tmtm.com>
2016-03-25 11:41:37 +0000

    Use webhook instead of notifier

--- scraper.rb
+++ scraper.rb
@@ -2,11 +2,9 @@
 # encoding: utf-8
 
 require 'wikidata/fetcher'
-require 'pry'
 
 names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://ro.wikipedia.org/wiki/Legislatura_2014-2018_(Republica_Moldova)',
   xpath: '//table[.//th[contains(.,"Nume")]]//tr[td]/td[2]//a[not(@class="new")]/@title',
 ) 
 EveryPolitician::Wikidata.scrape_wikidata(names: { ro: names })
-warn EveryPolitician::Wikidata.notify_rebuilder
