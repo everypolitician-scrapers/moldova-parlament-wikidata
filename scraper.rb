62eca18c5c368ad8e9ecfe4ff44b94b8872b103d
Tony Bowden <tony@tmtm.com>
2017-03-03 08:21:56 +0000

    Also fetch via Wikidata memberships

--- scraper.rb
+++ scraper.rb
@@ -3,8 +3,18 @@
 
 require 'wikidata/fetcher'
 
+sparq = <<EOS
+  SELECT ?item
+  WHERE {
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 wd:Q18390049 ;    # Member of the parliament of Moldova
+                        pq:P2937 wd:Q20431862 .  #   parliamentary term: XX
+  }
+EOS
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
 names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://ro.wikipedia.org/wiki/Legislatura_2014-2018_(Republica_Moldova)',
   xpath: '//table[.//th[contains(.,"Nume")]]//tr[td]/td[2]//a[not(@class="new")]/@title',
 ) 
-EveryPolitician::Wikidata.scrape_wikidata(names: { ro: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { ro: names })
